89cc169d6110c41d4e91a60526bf4f42055277a8
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 17:00:36 +0600

    added task-04.js file

---
+++ task-04.js
@@ -0,0 +1,34 @@
+/* Task-04 */
+const name = 'Gul bahar';
+const age = 21;
+const isMarried = false;
+const bioData = ['White', 38, true, 'Russian'];
+
+if (Array.isArray(name)) {
+    console.log('This variable is an array');
+}
+else {
+    console.log('This variable is not an array');
+}
+
+if (Array.isArray(age)) {
+    console.log('This variable is an array');
+}
+else {
+    console.log('This variable is not an array');
+}
+
+if (Array.isArray(isMarried)) {
+    console.log('This variable is an array');
+}
+else {
+    console.log('This variable is not an array');
+}
+
+if (Array.isArray(bioData)) {
+    console.log('This variable is an array');
+}
+else {
+    console.log('This variable is not an array');
+}
+
