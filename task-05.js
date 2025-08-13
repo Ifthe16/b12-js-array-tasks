a3ecdb531e193a616c4881f8a7ded0f2de95d60a
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 17:08:50 +0600

    added task-05.js file

---
+++ task-05.js
@@ -0,0 +1,8 @@
+/* Task-05 */
+const name = ['Mim', 'Sadia', 'Kona', 'Sumaiya', 'Suzana'];
+const num = [20, 21, 22, 23, 24];
+
+console.log(name, num);
+
+const combinedArr = name.concat(num);
+console.log(combinedArr);
