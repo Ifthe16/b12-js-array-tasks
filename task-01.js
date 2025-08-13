e986e2898a79a14341af8bca541f5be4ab6c1798
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 16:32:43 +0600

    added task-01.js and task-02.js files

---
+++ task-01.js
@@ -0,0 +1,8 @@
+/* Task-01 */
+const fruits = ['mango', 'banana', 'apple', 'strawberry', ' grape'];
+console.log(fruits);
+
+console.log(fruits[3]);
+
+fruits[2] = 'jambura';
+console.log(fruits);
---
+++ task-02.js
@@ -0,0 +1,15 @@
+/* Task-02 */
+const place = ['Bali', 'Barcelona', 'Maldives'];
+console.log(place);
+
+place.push('Hawaii');
+console.log(place);
+
+place.push("cox's bazar", 'Machu Picchu');
+console.log(place);
+
+const lastPlace = place.pop();
+console.log(lastPlace);
+
+console.log(place);
+
